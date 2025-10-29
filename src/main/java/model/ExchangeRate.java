9513ae129725cfd44d73567cd8748c86910b4aaf
dmitry <dmitryed6263@gmail.com>
2025-10-30 00:12:16 +0400

    class for currency pairs

---
+++ src/main/java/model/ExchangeRate.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class ExchangeRate {
+
+    // Класс для валютных пар
+
+    private long id;                    // айди
+    private Currency baseCurrency;      // изначальная валюта
+    private Currency targetCurrency;    // целевая валюта
+    private BigDecimal rate;            // курс
+
+    public ExchangeRate(long id, Currency baseCurrency, Currency targetCurrency, BigDecimal rate) {
+        this.id = id;
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Currency getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(Currency baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(Currency targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
