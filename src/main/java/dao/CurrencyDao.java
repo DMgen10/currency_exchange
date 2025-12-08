e79e1ab05c2929e3bb6a9227056ef9700617951e
dmitry <dmitryed6263@gmail.com>
2025-12-08 22:51:30 +0400

    created the interface - currency of DAO

---
+++ src/main/java/dao/CurrencyDao.java
@@ -0,0 +1,17 @@
+package dao;
+
+import model.Currency;
+import java.util.List;
+import java.util.Optional;
+
+public interface CurrencyDao {
+
+     List<Currency> findAll();
+
+     Optional<Currency> findByCode(String code);
+
+     Currency save(Currency currency);
+
+     Optional<Currency> findById(int id);
+
+}
