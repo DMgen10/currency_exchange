90b157ce23b06c353a3a94bf86ec289f4627bd0f
dmitry <dmitryed6263@gmail.com>
2025-12-08 22:39:12 +0400

    created a class for creating connections

---
+++ src/main/java/util/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConnection.class);
+
+    private DatabaseConnection() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:sqlite:currency_exchange.db";;
+        Connection connection = DriverManager.getConnection(url);
+
+        try(Statement statement = connection.createStatement()){
+            statement.execute("PRAGMA foreign_keys = ON");
+        } catch (SQLException exception){
+            log.error("error db", exception);
+
+        }
+        return connection;
+    }
+}
