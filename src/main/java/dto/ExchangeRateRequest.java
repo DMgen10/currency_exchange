ccf83a843c5a96015b55f831f3fe21431086caaa
dmitry <dmitryed6263@gmail.com>
2025-12-08 23:00:26 +0400

    created the exchange rate request class

---
+++ src/main/java/dto/ExchangeRateRequest.java
@@ -0,0 +1,34 @@
+package dto;
+
+import java.math.BigDecimal;
+
+public class ExchangeRateRequest {
+
+    private String baseCurrency;
+    private String targetCurrency;
+    private BigDecimal rate;
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public String getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(String targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
