987422e7e77848812094f7d52ecd553ba618cdeb
dmitry <dmitryed6263@gmail.com>
2025-12-08 22:57:49 +0400

    created the currency request class

---
+++ src/main/java/dto/CurrencyRequest.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class CurrencyRequest {
+
+    private String code;
+    private String name;
+    private String sign;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
